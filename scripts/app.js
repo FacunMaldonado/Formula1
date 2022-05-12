c073244adf08a6a1ef809f443a936b5e7e9a4078
FacunMaldonado <facu.sl94@gmail.com>
2022-05-12 15:12:54 -0300

    nav responsive home

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,12 @@
-nav. js :
-
-
-const navToggle = document.querySelector(".nav__nav-toggle");
-const navMenu = document.querySelector(".nav__menu");
-const navVisible = document.querySelector(".navVisible")
-const navimg = document.querySelector(".nav__list")
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
 
 navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("navVisible");
+  navMenu.classList.toggle("nav-menu_visible");
 
-  if (navMenu.classList.contains("navVisible")) {
-    navToggle.setAttribute("aria-label", "Cerrar menu");
-    navVisible.style.left= "0%";
-    navimg.style.background= "black";
-  } 
-  else {
-    navToggle.setAttribute("aria-label", "Abrir menu");
-    navVisible.style.left= "100%";
-    navimg.style.background= "transparent";
+  if (navMenu.classList.contains("nav-menu_visible")) {
+    navToggle.setAttribute("aria-label", "Cerrar menú");
+  } else {
+    navToggle.setAttribute("aria-label", "Abrir menú");
   }
 });
